7e3307a79510198e1f7f5d762ed4bac7f81bf86c
Sadiya <sadiyaayub16@gmail.com>
2023-05-22 10:42:23 +0530

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final int days =30;
-   final String name ="Sadiya";
+   final String name ="";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("Invitation App"),
       ),
         body:Center(
            child: Container(
-      child: Text("hey! here I am $days days back by $name"),
+      child: Text(""),
       ),
     ),
     drawer: Drawer(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,28 +9,33 @@ class loginpage extends StatefulWidget {
 class _loginpageState extends State<loginpage> {
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async{
+    if(_formKey.currentState!.validate()){
+    setState(() {
+                  changeButton = true;
+                });
+                await Future.delayed(Duration(seconds: 1));
+                await Navigator.pushNamed(context, MyRoutes.homeRoute );
+                setState(() {
+                  changeButton = false;
+                });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Material(
-    //   child:Center(
-    //     child:Text(
-    //       "Login Page",
-    //       style: TextStyle(
-    //         fontSize: 20,
-    //         Color:Colors.pinkAccent,
-    //         fontWeight: FontWeight.bold,
-    //       ),
-    //     ),
-    //   ),
-    // );
-          //textScaleFactor: 3.0,
-      // color:Colors.white,
-      child: Column(
+      color: Colors.white,
+      child:SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+         child: Column(
         children:[
-          Image.asset("rout/images/undraw_wedding_re_66hj.png",fit: BoxFit.cover,
+          Image.asset("rout/images/undraw_Events_re_98ue.png",fit: BoxFit.cover,
           ),
           SizedBox(
-            height: 20.0,
+            height: 15.0,
             ),
           Text(
             "Welcome $name ",
@@ -47,79 +52,76 @@ class _loginpageState extends State<loginpage> {
               padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
                 children: [
-           
               TextFormField(
               decoration: InputDecoration(
                 hintText: "Enter username",
                 labelText: "username",
                ), 
+               
+               validator: (value) {
+                if (value!.isEmpty) {
+                  return "Username cannot be empty";
+                }
+                return null;
+               },
                onChanged: (value) {
                  name = value;
-                 setState(() {
-                   
-                 });
+                 setState(() {});
                },
             ),
-
-
             TextFormField(
               obscureText: true,
               decoration: InputDecoration(
                 hintText: "Enter password",
                 labelText: "password",
                ),
+               validator: (value) {
+                if (value!.isEmpty) {
+                  return "password cannot be empty";
+                }
+                else if (value.length < 6) {
+                  return "password length should be atleast 6";
+                }
+                return null;
+               },
+             
             ),
             SizedBox(
-            height: 20.0,
+            height: 40.0,
             ),
-            InkWell(
-              onTap: () async {
-                setState(() {
-                  changeButton = true;
-                });
-                await Future.delayed((seconds :1));
-                Navigator.pushNamed(context, MyRoutes.homeRoute );
-              },
+            Material(
+              color: Colors.pinkAccent,
+               borderRadius: BorderRadius.circular(changeButton?50:8),
+            child : InkWell(
+              onTap: () => moveToHome(context),
               child:AnimatedContainer(
                 duration: Duration(seconds: 1),
               width: changeButton?50: 150,
               height: 50,
-              color: Colors.pinkAccent,
               alignment: Alignment.center,
-              child:changeButton?Icon(Icons.done,color: Colors.white,): Text(
+              child:changeButton
+              ?Icon(
+                Icons.done,
+                color: Colors.white,
+                ):
+                 Text(
                 "Login",
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 18),
               ),
-              decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(changeButton?50:8)
-              ),
+             
               ),
             ),
-
-            
-
-            // ElevatedButton( 
-            //   child:Text("Login"),
-            //   style: ButtonStyle(), 
-            // onPressed: () {
-            //   Navigator.pushNamed(context,MyRoutes.homeRoute);
-            // },
-            // )
+            ),
             ],
             ),
             )
+            
             ],
+      ),
+      ),
       ));
   }
 }
-
-
-
-
-
-
-         
